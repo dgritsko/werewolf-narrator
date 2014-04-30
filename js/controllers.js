c40290bb56ae3d75a203e60bfcf7a1021d227b49
Dan Gritsko <dgritsko@gmail.com>
2014-04-29 21:45:53 -0400

    Adding lots of roles and some reorganization of code

---
+++ js/controllers.js
@@ -0,0 +1,67 @@
+'use strict';
+
+werewolfApp.controller('RoleController', ['$scope', '$rootScope', 'roles', function($scope, $rootScope, roles) {
+   $scope.renderSection = true;
+
+   $scope.hasInstructions = function(actual, expected) {
+      return actual.instructions && actual.instructions.length;
+   }
+   
+   $scope.roles = roles;
+   
+   $scope.updateInstructions = function() {
+      $scope.renderSection = false;
+   
+      var instructions = [];
+      
+      for (var i = 0, len = $scope.roles.length; i < len; i++) {
+         var role = $scope.roles[i];
+         
+         if (role.count <= 0) { continue; } 
+         
+         instructions = instructions.concat(role.instructions);         
+      }
+     
+      $rootScope.$broadcast('updateInstructions', {instructions: instructions});
+   };   
+   
+   $rootScope.$on('defineRoles', function(e, args) {
+      $scope.renderSection = true;
+   });
+   
+   $scope.increaseCount = function(role) {
+      role.count = role.maxCount ? Math.min(role.maxCount, role.count + 1) : role.count + 1;
+   };
+   
+   $scope.decreaseCount = function(role) {
+      role.count = Math.max(0, role.count - 1);
+   }
+}]);
+
+werewolfApp.controller('ScriptController', ['$scope', '$rootScope', function($scope, $rootScope) {
+   $scope.renderSection = false;
+   $scope.instructions = [];
+   $scope.round = 0;
+   
+   $scope.activeInRound = function(actual, expected) {
+      if (!actual.rounds || actual.rounds.length == 0) {
+         return true;
+      }
+      
+      if (actual.rounds.indexOf($scope.round) >= 0) { 
+         return true;
+      }
+      
+      return false;
+   }
+   
+   $rootScope.$on('updateInstructions', function(e, args) {
+      $scope.renderSection = true;
+      $scope.instructions = args.instructions;
+   });
+   
+   $scope.defineRoles = function() {
+      $scope.renderSection = false;
+      $rootScope.$broadcast('defineRoles', {});
+   }
+}]);
---
+++ js/factories.js
@@ -0,0 +1,95 @@
+'use strict';
+
+werewolfApp.factory('roles', function() {
+var makeRole = function(name, instructions, extraArgs) {
+   var role = {
+      name: name,
+      count: 0,
+      instructions: instructions || []
+   };
+   
+   if (extraArgs) {
+      for(var k in extraArgs) {
+         role[k] = extraArgs[k];
+      }
+   }
+   
+   return role;
+};
+
+var makeInstruction = function(message, rounds) {
+   return {
+      message: message,
+      rounds: rounds || []
+   }
+};
+
+return [
+makeRole('Apprentice Seer'),
+makeRole('Aura Seer'),
+makeRole('Bodyguard',
+   [
+      makeInstruction('Bodyguard, wake up.'),
+      makeInstruction('Bodyguard, choose who you would like to protect.'),
+      makeInstruction('Bodyguard, go to sleep.')
+   ]),
+makeRole('Cupid',
+   [
+      makeInstruction('Cupid, wake up.', [0]),
+      makeInstruction('Cupid, choose the two lovers.', [0]),
+      makeInstruction('Cupid, go to sleep.', [0])      
+   ], { 'maxCount' : 1 } ),
+makeRole('Diseased'),
+makeRole('Ghost'),
+makeRole('Hunter'),
+makeRole('Idiot'),
+makeRole('Lycan'),
+makeRole('Magician'),
+makeRole('Martyr'),
+makeRole('Mason',
+   [
+      makeInstruction('Masons, wake up.', [0]),
+      makeInstruction('Masons, identify each other.', [0]),
+      makeInstruction('Masons, go to sleep.', [0]),      
+   ]),
+makeRole('Mayor'),
+makeRole('Old Hag'),
+makeRole('Old Man'),
+makeRole('P.I.'),
+makeRole('Pacifist'),
+makeRole('Priest'),
+makeRole('Prince'),
+makeRole('Seer', 
+   [
+      makeInstruction('Seer, wake up.'),
+      makeInstruction('Seer, choose your suspect.'),
+      makeInstruction('Seer, go to sleep.')      
+   ], { 'maxCount' : 1 } ),
+makeRole('Spellcaster'),
+makeRole('Tough Guy'),
+makeRole('Troublemaker'),
+makeRole('Villager'),
+makeRole('Witch'),
+makeRole('Sorcerer'),
+makeRole('Minion'),
+makeRole('Werewolf',
+   [  
+      makeInstruction('Werewolves, wake up.'),
+      makeInstruction('Werewolves, identify each other.', [0]),
+      makeInstruction('Werewolves, choose your victim.'),
+      makeInstruction('Werewolves, go to sleep.')
+   ]),
+makeRole('Wolf Cub'),
+makeRole('Cursed'),
+makeRole('Doppelganger'),
+makeRole('Drunk'),
+makeRole('Lovers'),
+makeRole('Cult Leader'),
+makeRole('Hoodlum'),
+makeRole('Tanner'),
+makeRole('Lone Wolf'),
+makeRole('Vampire'),
+makeRole('Amulet of Protection'),
+makeRole('Villager')
+];
+});
